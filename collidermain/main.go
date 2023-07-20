42b39e45349e8bcef1c2a54a71e6b6451cbc97ce
Faker0715 <949506563@qq.com>
2023-07-20 22:25:28 +0800

    [feat] change file authority

--- collidermain/main.go
+++ collidermain/main.go
@@ -11,6 +11,13 @@ import (
 	"log"
 )
 
+
+
+
+
+
+
+
 var tls = flag.Bool("tls", true, "whether TLS is used")
 var port = flag.Int("port", 443, "The TCP port that the server listens on")
 var roomSrv = flag.String("room-server", "https://appr.tc", "The origin of the room server")
